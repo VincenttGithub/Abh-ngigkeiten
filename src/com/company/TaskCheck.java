0623aa643856b4d94521b260f6b5bd02ccf664f8
V <64865979+Vincent13122@users.noreply.github.com>
2020-05-05 23:32:31 +0200

    ddd

--- src/com/company/TaskCheck.java
+++ src/com/company/TaskCheck.java
@@ -5,6 +5,8 @@ public class TaskCheck
     public void isWellSorted(String[] sequence)
     {
         System.out.println("Hello World");
+        System.out.println("Hello Worldd");
+        System.out.println("Hello Worlddd");
 
     }
 }
